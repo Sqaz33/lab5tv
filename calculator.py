5e0267d551245e4d5fc4bc67f765a41a764f6b4f
DearDorothy <deardorothy@mail.ru>
2024-04-25 23:45:35 +0300

    test_seaborn

--- calculator.py
+++ calculator.py
@@ -1,35 +1,24 @@
-# class TestClass:
-    
-#     def __init__(self, arg1, arg2):
-#         self.arg1 = arg1
-#         self.arg2 = arg2
-        
-#     def method1(self):
-#         print("1", self.arg1)
-        
-#     def method2(self):
-#         print("2", self.arg2)
-        
 
-# obj = TestClass("arg1", "arg2")
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-# obj.method1()
-# obj.method2()
+sns.set_style("whitegrid")
 
-import seaborn
+x = range(20)
+y = [i ** 2 for i in x]
 
-class Calculator:
-    
-    
-    
-    
-    def solveB1(self, data: list[int]) -> list[int]:
-        pass
-    def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], graph):
-        pass
-    
-    
+graph = plt.plot(x, y)
 
 
-if __name__ == "__main__":
-    pass
+# class Calculator:
+#
+#     def solveB1(self, data: list[int]) -> list[int]:
+#         pass
+#     def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], graph):
+#         pass
+#
+
+
+
+
