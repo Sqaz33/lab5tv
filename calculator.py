81caa23ee9f4c33724ca439795698a91a874bb99
zhanet <mset321@gmail.com>
2024-04-26 17:12:45 +0300

    работаем над Calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
 
-import seaborn as sns
-# import matplotlib as plt
-# import random
 
 class Calculator:
 
@@ -11,7 +10,7 @@ class Calculator:
     # def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], list[float]):
     #     pass
 
-    def solveC1(self, data: list[float]): #-> tuple(list[list[float]], list[list[float]], sns, sns):
+    def solveC1(self, data: list[float]) -> tuple[PrettyTable, PrettyTable]:
         count = dict()
         for i in data:
             count[i] = count.get(i, 0) + 1
@@ -22,10 +21,31 @@ class Calculator:
 
         relative_freq = [i / sum(freq) for i in freq]
 
+        var = self.solveB1(data)
+        # Построение графиков
+        fig1, pol1 = plt.subplots()
+        fig1, pol2 = plt.subplots()
 
+        pol1.plot(var, freq)
+        pol1.set_title("Полигон частот")
+
+        pol2.plot(var, relative_freq)
+        pol2.set_title("Полигон относительных частот")
+
+        # Построение таблиц
+        freq_table = PrettyTable()
+        freq_table.add_column("Xi", var)
+        freq_table.add_column("Ni", freq)
+
+        relative_freq_table = PrettyTable()
+        relative_freq_table.add_column("Xi", var)
+        relative_freq_table.add_column("Wi", [f'{i:.3f}' for i in relative_freq])
+
+        return freq_table, relative_freq_table
 
     # def solveC2(self, data: list[float]) -> tuple(list[float], list[float], graph):
-    # def solveD1:
+    def solveD1(self):
+        pass
     # def solveD2:
     # def solveE1::
     # def solveE2:
@@ -33,12 +53,9 @@ class Calculator:
 
 if __name__ == "__main__":
     # calc = Calculator()
-    # print(calc.solveC1([1, 3, 1, 3, 3, 2, 2, 2, 5, 4, 4]))
-
-    fmri = sns.load_dataset("fmri")
-    sns.relplot(
-        data=fmri, kind="line",
-        x="timepoint", y="signal", col="region",
-        hue="event", style="event",
-    )
-    sns.catplot(data=[1, 3, 1, 3, 3, 2, 2, 2, 5, 4, 4], kind="bar", x="day", y="total_bill", hue="smoker")
+    # for t in calc.solveC1([1, 3, 1, 3, 3, 2, 2, 2, 5, 4, 4, 4, 4, 4, 4, 4]):
+    #     print(t)
+    x = [1, 4, 4, 6, 6, 16]
+    y = [0.2, 0.2, 0.5, 0.5, 1, 1]
+    plt.plot(x, y)
+    plt.show()
