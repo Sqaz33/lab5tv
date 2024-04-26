ed6ae97ce5140cd7c18d8ead33d8182618dcc383
zhanet <mset321@gmail.com>
2024-04-26 14:28:24 +0300

    работаем над solveb1

--- calculator.py
+++ calculator.py
@@ -1,17 +1,14 @@
 
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import matplotlib as plt
-import random
-
-
+# import seaborn as sns
+# import pandas as pd
+# import numpy as np
+# import matplotlib as plt
+# import random
 
 class Calculator:
 
     def solveB1(self, data: list[int]) -> list[int]:
-
-        return sorted(list[int])
+        return list(sorted(set(data)))
 
     # def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], graph):
     #     pass
@@ -24,3 +21,8 @@ class Calculator:
     # def solveD2:
     # def solveE1::
     # def solveE2:
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    print(calc.solveB1([1, 1, 3, 3, 3, 2, 2, 2, 5, 4, 4]))
