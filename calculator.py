1d455bbc83ad1cd6de90e97faf1fe5a374809129
DearDorothy <deardorothy@mail.ru>
2024-04-26 14:17:32 +0300

    Работаем над выч. блоком

--- calculator.py
+++ calculator.py
@@ -1,24 +1,27 @@
 
 import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import matplotlib as plt
+import random
 
-sns.set_style("whitegrid")
-
-x = range(20)
-y = [i ** 2 for i in x]
-
-graph = plt.plot(x, y)
+class
 
 
-# class Calculator:
-#
-#     def solveB1(self, data: list[int]) -> list[int]:
-#         pass
-#     def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], graph):
-#         pass
-#
+class Calculator:
 
+    def solveB1(self, data: list[int]) -> list[int]:
 
+        return sorted(list[int])
 
-
+    # def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], graph):
+    #     pass
+    #
+    # def solveC1(self, data: list[float]) -> tuple(list[float], list[float], graph):
+    #     pass
+    #
+    # def solveC2(self, data: list[float]) -> tuple(list[float], list[float], graph):
+    # def solveD1:
+    # def solveD2:
+    # def solveE1::
+    # def solveE2:
