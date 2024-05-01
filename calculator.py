51727ede0647c4d73d8c2d341192a6382cca4d39
DearDorothy <deardorothy@mail.ru>
2024-05-01 22:22:30 +0300

    Добавили три метода: solveD1, get_frequencies, get_relative_frequencies

--- calculator.py
+++ calculator.py
@@ -1,25 +1,29 @@
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 
-
 class Calculator:
 
-    def solveB1(self, data: list[float]) -> list[float]:
-        return list(sorted(set(data)))
-
-    # def solveB2(seld, data: list[float], number_of_interval: int) -> tuple(list[float], list[float], list[float]):
-    #     pass
-
-    def solveC1(self, data: list[float]) -> tuple[PrettyTable, PrettyTable]:
+    def get_frequencies(self, data: list[float]) -> list[int]:
         count = dict()
         for i in data:
             count[i] = count.get(i, 0) + 1
-
         freq = []
         for i in sorted(count.keys()):
             freq.append(count[i])
+        return freq
 
-        relative_freq = [i / sum(freq) for i in freq]
+    def get_relative_frequencies(self, data: list[float]) -> list[float]:
+        freq = self.get_frequencies(data)
+        return [i / sum(freq) for i in freq]
+
+    def solveB1(self, data: list[float]) -> list[float]:
+
+        return list(sorted(set(data)))
+
+    def solveC1(self, data: list[float]) -> tuple[PrettyTable, PrettyTable]:
+
+        freq = self.get_frequencies(data)
+        relative_freq = self.get_relative_frequencies(data)
 
         var = self.solveB1(data)
         # Построение графиков
@@ -44,18 +48,37 @@ class Calculator:
         return freq_table, relative_freq_table
 
     # def solveC2(self, data: list[float]) -> tuple(list[float], list[float], graph):
-    def solveD1(self):
-        pass
+    def solveD1(self, data: list[float]) -> tuple[str, list[str]]:
+        func_definition = ("Эмпирической функцией распредления называют функцию F*(x), определяющую для каждого значения x "
+                           "относительну частоту события X < x: F*(x) = n_x/n")
+
+        X = self.solveB1(data)
+        P = self.get_relative_frequencies(data)
+        p = 0
+        Fx = [0] + [p := p + i for i in P]
+        Fx = [f'{p:0.2f}' for p in Fx]
+
+        intervals = [f'x < {X[0]}']
+        for i in range(0, len(X) - 1):
+            intervals.append(f'{X[i]} < x <= {X[i + 1]}')
+        intervals += [f'x > {X[-1]}']
+
+        ans = [f'если {xs} то F*(x) = {p}' for p, xs in zip(Fx, intervals)]
+
+        return func_definition, ans
+
     # def solveD2:
     # def solveE1::
     # def solveE2:
 
 
 if __name__ == "__main__":
-    # calc = Calculator()
-    # for t in calc.solveC1([1, 3, 1, 3, 3, 2, 2, 2, 5, 4, 4, 4, 4, 4, 4, 4]):
-    #     print(t)
-    x = [1, 4, 4, 6, 6, 16]
-    y = [0.2, 0.2, 0.5, 0.5, 1, 1]
-    plt.plot(x, y)
-    plt.show()
+    calc = Calculator()
+    data = [1, 4, 4, 6, 6, 16]
+    # print(calc.solveC1(data))
+    # ans = calc.solveD1(data)
+    # print(ans[0])
+    # for s in ans[1]:
+    #     print(s)
+
+
