9ead7932ddc5626696fa3262725f76e3a7e16d89
Matveev Stepan Andreevich <mset321@gmail.com>
2024-05-03 00:29:56 +0300

    Решил задачи B2, C2

--- calculator.py
+++ calculator.py
@@ -82,6 +82,45 @@ class Calculator:
         n = sum(frequencies)
         return n / (n - 1) * self.dispersion(variation_series, frequencies)
 
+    def get_intervals(self, data: list[float], interval_number: int) -> list[list[int]]:
+        var = self.solveB1(data)
+        mx = max(var)
+        mn = min(var)
+        step = (mx - mn) / interval_number
+        intervals = []
+        x = mn
+        while x + step <= mx:
+            intervals.append([x, x := x + step])
+        return intervals
+    
+    def get_intervals_frequencies(self, data: list[float], interval_number: int) -> list[int]:
+        var = self.solveB1(data)
+        var_freq = self.get_frequencies(data)
+        intervals = self.get_intervals(data, interval_number)
+        freq = [0] * interval_number
+        for i in range(interval_number):
+            for j in range(len(var)):
+                if intervals[i][0] <= var[j] < intervals[i][1]:
+                    freq[i] += var_freq[j]
+        for i in range(len(var)):
+            if var[i] == intervals[-1][1]:
+                freq[-1] += var_freq[i]
+        return freq
+    
+    def get_intervals_relative_frequencies(self, data: list[float], interval_number: int) -> list[float]:
+        var = self.solveB1(data)
+        var_rel_freq = self.get_relative_frequencies(data)
+        intervals = self.get_intervals(data, interval_number)
+        rel_freq = [0] * interval_number
+        for i in range(interval_number):
+            for j in range(len(var)):
+                if intervals[i][0] <= var[j] < intervals[i][1]:
+                    rel_freq[i] += var_rel_freq[j]
+        for i in range(len(var)):
+            if var[i] == intervals[-1][1]:
+                rel_freq[-1] += var_rel_freq[i]
+        return rel_freq
+    
     # -------------решения задач-------------------------
     def solveB1(self, data: list[float]) -> list[float]:
         """
@@ -92,8 +131,29 @@ class Calculator:
         """
         return list(sorted(set(data)))
 
-    def solveB2(self, data: list[float], intervals_number: int):
-        raise "234"
+    def solveB2(self, data: list[float], interval_number: int) -> PrettyTable:
+        assert interval_number > 0, "Невозмножно построить интервалы"
+        
+        var = self.solveB1(data)
+        
+        # Получение интервалов
+        intervals = self.get_intervals(data, interval_number)
+        
+        # Получение интервального ряда частот и относ. частот
+        freq = self.get_intervals_frequencies(data, interval_number)
+        rel_freq = self.get_intervals_relative_frequencies(data, interval_number)
+
+        # Построение таблицы
+        table = PrettyTable()
+        table.add_column("Номер интервала - i", [i for i in range(1, interval_number+1)])
+        table.add_column("Частичный интервал", [f'{i[0]:0.3f}-{i[1]:0.3f}' for i in intervals])
+        table.add_column("Сумма частот интервала", freq)
+        table.add_column("Сумма относительных частот интервала", [f'{i:0.3f}' for i in rel_freq])
+
+        # Построение гистрограмм
+        plt.hist(data, bins=interval_number, color='skyblue', edgecolor='black')
+        plt.title("Гистограмма")
+        return table
 
     def solveC1(self, data: list[float]) -> tuple[PrettyTable, PrettyTable]:
         """
@@ -129,8 +189,28 @@ class Calculator:
 
         return freq_table, relative_freq_table
 
-    def solveC2(self, data: list[float]) -> tuple[list[float], list[float]]:
-        pass
+    def solveC2(self, data: list[float], interval_number: int) -> PrettyTable:
+        middle = [(i[0]+i[1]) / 2 for i in self.get_intervals(data, interval_number)]
+        # Получение группированного ряда распределения 
+        freq = self.get_intervals_frequencies(data, interval_number)
+        relative_freq = self.get_intervals_relative_frequencies(data, interval_number)
+        
+        table = PrettyTable()
+        table.add_column("Середина интервала", [f'{i:0.3f}' for i in middle])
+        table.add_column("Частоты", freq)
+        table.add_column("Относительных частоты", [f'{i:0.3f}' for i in relative_freq])
+
+        # Построение полигонов
+        fig1, pol1 = plt.subplots()
+        fig1, pol2 = plt.subplots()
+
+        pol1.plot(middle, freq)
+        pol1.set_title("Полигон частот")
+
+        pol2.plot(middle, relative_freq)
+        pol2.set_title("Полигон относительных частот")
+
+        return table
 
     def solveD1(self, data: list[float]) -> tuple[str, list[str]]:
         """
@@ -171,7 +251,9 @@ class Calculator:
             )
             # добавление пунктира
         for i in range(len(Fx) - 1):
-            plt.plot([intervals[i][1], intervals[i][1]], [Fx[i], Fx[i + 1]], linestyle="--", color="black")
+            plt.plot(
+                [intervals[i][1], intervals[i][1]], [Fx[i], Fx[i + 1]], linestyle="--", color="black"
+                )
 
         # получаем строковое представленние функции
         str_intervals = ([f'x <= {X[0]}']
@@ -211,13 +293,8 @@ class Calculator:
 
 
 if __name__ == "__main__":
-    # calc = Calculator()
-    # data = []
 
-    # # Отображение графика
-    # ans = calc.solveD1(data)
-    # print(ans[0])
-    # for s in ans[1]:
-    #     print(s)
-    # plt.show()
-    Calculator().solveB2([], 123)
+    data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
+
+    print(Calculator().solveC2(data, 10))
+    plt.show()
