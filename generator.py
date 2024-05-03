a961aaee94be80546403d22577359c0ce6043b2e
Matveev Stepan Andreevich <mset321@gmail.com>
2024-05-03 12:17:13 +0300

    добавил генератор случ. списка чисел

---
+++ generator.py
@@ -0,0 +1,44 @@
+import random
+
+
+class Generator:
+    """
+        3. Составить файл из статистических данных, содержащий от 50 до 80
+        целых чисел, среди которых количество различных вариантов должно
+        быть не менее 10 и не более 20 (числа могут быть как одного, так и разных
+        знаков).
+    """
+    @staticmethod
+    def generate_list_of_number(
+            amount_of_numbers: int,
+            values: tuple[int, int],
+            freq: tuple[int, int]
+    ) -> list[...]:
+        # составить вар. ряд с частотами
+        rands = dict()
+        sum_freq = 0
+        while sum_freq < amount_of_numbers:
+            rand_val = random.randint(values[0], values[1])
+            rand_freq = random.randint(freq[0], freq[1])
+            if rands.get(rand_val, "not") == "not":
+                rands[rand_val] = rand_freq
+                sum_freq += rand_freq
+
+        # подбить сумму частот до треб. кол. чисел
+        # оч медленное решение ------------------
+        while sum_freq != amount_of_numbers:
+            for n in rands.keys():
+                if sum_freq == amount_of_numbers:
+                    break
+                if rands[n] >= freq[0] + 1:
+                    rands[n] -= 1
+                    sum_freq -= 1
+        # ---------------------------------------
+
+        # составить список случайных чисел
+        stats = []
+        for n in rands.keys():
+            stats += [n] * rands[n]
+        random.shuffle(stats)
+
+        return stats
---
+++ main.py
@@ -0,0 +1,6 @@
+from calculator import Calculator
+from generator import Generator
+
+
+if __name__ == "__main__":
+    pass
